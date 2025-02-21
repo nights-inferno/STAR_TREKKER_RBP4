300a32362b2a442dfa25867eb2429c7d5c8af47a
nights-inferno <anastasia.deych@epfl.ch>
2025-02-21 10:49:00 +0100

    Update subscriber.py
    Started reading controller data

--- rover_commands/rover_commands/subscriber.py
+++ rover_commands/rover_commands/subscriber.py
@@ -14,37 +14,8 @@ class TrajectorySubscriber(Node):
         self.position = {'x': 0.0, 'z': 0.0, 'ry': 0.0}
 
     def listener_callback(self, msg : Twist):
-        if msg.linear.z and msg.angular.y or msg.linear.z and msg.linear.x :
-            print("Forbidden move")
-        else :
-            if msg.linear.x > 0 and not msg.angular.y :
-                print("Go Forward")
-            if msg.linear.x < 0 and not msg.angular.y :
-                print("Go Backward")
-            
-            if msg.linear.x*msg.angular.y > 0  :
-                print("Go Right")
-            if msg.linear.x*msg.angular.y < 0 :
-                print("Go Left")
-
-            if msg.angular.y > 0 and not msg.linear.x :
-                print("Rotating on itself to the Left")
-            if msg.angular.y < 0 and not msg.linear.x :
-                print("Rotating on itself to the Right")
-            
-            if msg.linear.z > 0 :
-                print("Slide Right")
-            if msg.linear.z < 0 :
-                print("Slide Left")
-        
-        self.position['ry'] += msg.angular.y
-        self.position['z'] += msg.linear.z
-        self.position['x'] += msg.linear.x
-       
-        # TODO: Interpret the received commands and log the result using self.get_logger().info()
-        # Your code here
-
-        self.get_logger().info(f'New Position: {self.position}')
+               
+        self.get_logger().info(f'Status: {msg.linear}, and {msg.angular}')
 
 def main(args=None):
     rclpy.init(args=args)
